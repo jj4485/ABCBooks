8737fc4a0c7050e93084802a9753a125d13e341e
Jonathan <jj4485@princeton.edu>
2024-05-05 20:28:32 -0400

    done

--- embeddings.py
+++ embeddings.py
@@ -1,11 +1,12 @@
 import json
-import gensim.downloader as gensim
+import gensim.downloader as api
 import random
 from pathlib import Path
 import pandas as pd
-from sklearn.decomposition import PCA
+from sklearn.manifold import TSNE
 from sklearn.preprocessing import StandardScaler
 from sklearn.cluster import KMeans
+import matplotlib.pyplot as plt
 
 ROOT_DIR = Path(__file__).parent
 TEXT_FILE = ROOT_DIR / 'words.txt'
@@ -18,27 +19,28 @@ pd.set_option('display.max_rows', None)
 pd.set_option('display.max_columns', None)
 # Extract a list of words
 words = [item['term'] for item in data]
-print(words)
+
 
 # Set a seed for reproducibility
-#random.seed(42)
+random.seed(42)
 
 # Randomly select 1000 words
-#random_words = random.sample(words, 1000)
+random_words = random.sample(words, 1000)
 
 # Load the GloVe model
-model = gensim.load('glove-wiki-gigaword-100')
+model = api.load('glove-wiki-gigaword-100')
 
 # Get embeddings for selected words
-embeddings = {word: model[word] for word in words}
+embeddings = {word: model[word] for word in random_words if word in model}
 
 # Create a DataFrame from embeddings
 df = pd.DataFrame.from_dict(embeddings, orient='index')
 
 
+
 # Correctly using the DataFrame's index to get the word labels
 labels = df.index
-#print(labels)
+###print(labels)
 
 features = df.iloc[:, :]
 #print(features)
@@ -47,9 +49,20 @@ scaler = StandardScaler()
 features_scaled = scaler.fit_transform(features)
 
 
-kmeans = KMeans(n_clusters=3, n_init=10)
+kmeans = KMeans(n_clusters=25, n_init=10)
 clusters = kmeans.fit_predict(features_scaled)
 
-# Now correctly including word labels in the clustered data
+
 clustered_data = pd.DataFrame({'Label': labels, 'Cluster': clusters})
 print(clustered_data)
+
+tsne = TSNE(n_components=3, verbose=1, perplexity=40, n_iter=300)
+tsne_results = tsne.fit_transform(features_scaled)
+
+# Plotting the results of t-SNE with cluster labels in 3D
+fig = plt.figure(figsize=(10, 8))
+ax = fig.add_subplot(111, projection='3d')
+scatter = ax.scatter(tsne_results[:, 0], tsne_results[:, 1], tsne_results[:, 2], c=clusters, cmap='viridis', alpha=0.5)
+plt.title('3D t-SNE visualization of word embeddings clustered into 5 groups')
+fig.colorbar(scatter)
+plt.show() 
